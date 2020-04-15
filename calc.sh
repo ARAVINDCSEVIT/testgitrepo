72fdc9b484eba2461e6768c094e4f5994b115a19
Your Name <aravindcse201820@gmail.com>
2020-04-15 20:08:45 +0530

    added calc appln

--- calc.sh
+++ calc.sh
@@ -2,4 +2,10 @@ echo "Calculator Application"
 echo "Number1:$Num1"
 echo "Number2:$Num2"
 add=$(($Num1 + $Num2))
-echo "Additon:$add"
+sub=$(($Num1 - $Num2))
+mul=$(($Num1 * $Num2))
+div=$(($Num1 / $Num2))
+echo "Addition:$add"
+echo "Subtraction:$sub"
+echo "Multiplication:$mul"
+echo "Division:$div"
